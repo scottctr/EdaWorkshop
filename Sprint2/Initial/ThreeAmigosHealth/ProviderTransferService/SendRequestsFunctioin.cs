312f97a907b2b23496c22478d8b875012a7a063a
scott.carter <scott.carter@evicore.com>
2021-09-19 17:11:02 -0500

    More Sprint 1 and slide tweaks

--- Sprint2/Initial/ThreeAmigosHealth/ProviderTransferService/SendRequestsFunctioin.cs
+++ Sprint2/Initial/ThreeAmigosHealth/ProviderTransferService/SendRequestsFunctioin.cs
@@ -1,13 +1,8 @@
 using Azure.Messaging.EventHubs;
 using Azure.Messaging.EventHubs.Producer;
-using BusinessLogic;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.Json;
 
 namespace ProviderTransferServiceFunction
 {
@@ -20,45 +15,19 @@ namespace ProviderTransferServiceFunction
         // "* * * * * *" fires every second
         // "* * * * *" fires every minute
         [FunctionName("SendRequestsFunction")]
-        public static async void Run([TimerTrigger("* * * * * *")]TimerInfo myTimer, ILogger log, ExecutionContext context)
+        public static async void Run([TimerTrigger("* * * * * *")]TimerInfo myTimer, ILogger log)
         {
-            var config = GetConfig(context);
-
             _publisher = new EventHubProducerClient(_connectionString);
 
-            // We can set batchSize to send more requests per iteration and demo scalability 
-            // Don't forget to set batchSize in local.settings.json and publish settings
-            var batchSize = int.Parse(config["batchSize"]);
-            await _publisher.SendAsync(GetRequests(batchSize));
-        }
+            var requestList = new System.Collections.Generic.List<EventData>();
+            var request = BusinessLogic.RequestGenerator.GetRandomRequest();
+            var requestJson = System.Text.Json.JsonSerializer.Serialize(request);
+            var requestBytes = System.Text.Encoding.UTF8.GetBytes(requestJson);
 
-        private static List<EventData> GetRequests(int count)
-        {
-            var requestList = new List<EventData>(count);
-            for(var i = 0; i < count; i++)
-            {
-                var request = RequestGenerator.GetRandomRequest();
-                var requestJson = JsonSerializer.Serialize(request);
+            requestList.Add(new EventData(new BinaryData(requestBytes)));
+            Console.WriteLine("Request received: " + requestJson);
 
-                var requestBytes = Encoding.UTF8.GetBytes(requestJson);
-                requestList.Add(new EventData(new BinaryData(requestBytes)));
-                Console.WriteLine($"Request {i} received: " + requestJson);
-            }
-
-            return requestList;
-        }
-
-        private static IConfigurationRoot GetConfig(ExecutionContext context)
-        {
-            return new ConfigurationBuilder()
-                    .SetBasePath(context.FunctionAppDirectory)
-                    // This gives you access to your application settings 
-                    // in your local development environment
-                    .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                    // This is what actually gets you the 
-                    // application settings in Azure
-                    .AddEnvironmentVariables()
-                    .Build();
+            await _publisher.SendAsync(requestList);
         }
     }
 }
